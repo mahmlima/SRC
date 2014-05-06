28b077115a30f2f6defc03ae2fd9b8cd796de0ff
Marina <mahmlima@gmail.com>
2014-05-06 13:32:05 -0700

    editing css/html on list_of_articles

--- meu_blog/blog/views.py
+++ meu_blog/blog/views.py
@@ -4,8 +4,12 @@ from django.template import RequestContext
 
 from models import Artigo
 
+
+import datetime
+
 def list_of_articles(request):
 	artigos = Artigo.objects.all()
+	now = datetime.datetime.today()
 	return render_to_response('blog/list_of_articles.html',
 		locals(), context_instance=RequestContext(request)
 		#{'artigos':artigos}
@@ -14,6 +18,7 @@ def list_of_articles(request):
 
 def article(request,artigo_id):
 	artigo = Artigo.objects.get(id=artigo_id)
+	
 	return render_to_response('blog/artigo.html',locals(),context_instance=RequestContext(request))
 
 
